e7d49c5a1478c1a90b3092863663d6096d407a34
HexMakina <20891495+HexMakina@users.noreply.github.com>
2026-04-14 12:02:52 +0200

    login PHP avec variables

---
+++ connect.php
@@ -0,0 +1,17 @@
+<?php
+
+var_dump($_POST['username']);
+var_dump($_POST['password']);
+
+$db_username = 'sam@amstram.be';   // viendra de la db
+$db_password = 'moncode';          // viendra de la db
+
+if($db_username === $_POST['username'] && $db_password === $_POST['password']){
+    // aller sur dasbhoard
+    header('Location: dashboard.html');
+}else{
+    // aller sur login avec message d'erreur
+    header('Location: login.php?error=1');
+}
+
+?>
--- login.php
+++ login.php
@@ -6,13 +6,18 @@
 </head>
 <body>
     <h1>Connexion</h1>
+    <?php
+    if(isset($_GET['error'])){
+        echo '<p>Failed. Retry.</p>';
+    }
+    ?>
 
-    <form method="POST" action="handler.php">
+    <form method="POST" action="connect.php">
         <label for="username">Nom d'utilisateur</label>
-        <input id="username" name="username" required>
+        <input id="username" name="username" value="sam@amstram.be" required>
 
         <label for="password">Mot de passe</label>
-        <input id="password" name="password" type="password" required>
+        <input id="password" name="password" value="moncode" type="password" required>
 
         <button type="submit">Se connecter</button>
     </form>
