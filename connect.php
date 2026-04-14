fd1dd1822bc6a3007e9668a8110b825bab499a63
HexMakina <20891495+HexMakina@users.noreply.github.com>
2026-04-14 12:07:25 +0200

    dashboard is now php

--- connect.php
+++ connect.php
@@ -8,7 +8,7 @@ $db_password = 'moncode';          // viendra de la db
 
 if($db_username === $_POST['username'] && $db_password === $_POST['password']){
     // aller sur dasbhoard
-    header('Location: dashboard.html');
+    header('Location: dashboard.php');
 }else{
     // aller sur login avec message d'erreur
     header('Location: login.php?error=1');
